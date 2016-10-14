9238cdef9e9ec9f8ec6e08d8b120d08c7866b2f9
tony <tonydelacruzjr@gmail.com>
2016-10-14 14:29:04 -0700

    player script
    finish the player script,audio,flashlight

---
+++ Assets/scripts/FlashLight.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashLight : MonoBehaviour
+{
+
+    Light spotLight;
+
+    public bool lightOn;
+
+    public AudioSource soundOn;
+    public AudioSource soundOff;
+
+	// Use this for initialization
+	void Start ()
+    {
+        spotLight = GetComponent<Light>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            lightOn = !lightOn;
+        }
+                
+        if(lightOn)
+        {
+            spotLight.enabled = true;
+            soundOn.Play();
+            
+        }
+
+        else if(!lightOn)
+        {
+            spotLight.enabled = false;
+            soundOff.Play();
+        }
+    }  
+}
