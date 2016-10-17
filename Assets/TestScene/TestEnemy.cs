9ad8b1dcd32fd1c0205430dabc07574f80c41a44
DmoneyLuffey <DmoneyLuffey@gmail.com>
2016-10-17 09:56:28 -0700

    Static Effect

---
+++ Assets/TestScene/TestEnemy.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestEnemy : MonoBehaviour
+{
+    public Transform target;
+    public TestHealthScript healthLoss;
+    public int moveSpeed;
+    public int rotationSpeed;
+    public int maxdistance;
+    private Transform myTransform;
+    //------------------------------------//    
+
+    void Awake()
+    {
+        myTransform = transform;
+    }
+
+
+    void Start()
+    {
+
+        maxdistance = 2;
+    }
+
+
+    void Update()
+    {
+
+
+        if (Vector3.Distance(target.position, myTransform.position) > maxdistance)
+        {
+            //Move towards target
+            transform.LookAt(target.position);
+            
+            myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+
+        }
+    }
+}
--- Assets/TestScene/TestHealthScript.cs
+++ Assets/TestScene/TestHealthScript.cs
@@ -1,20 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Rendering;
+    
 
 public class TestHealthScript : MonoBehaviour
 {
-    public int health = 100;
-
+    public float health = 100f;
+    private float startingHealth;
+    public float healthDecrease = 5.0f;
+    
     public Renderer staticRenderer;
 
 	// Use this for initialization
 	void Start ()
     {
+        startingHealth = health;
+
         GameObject staticObject = GameObject.Find("StaticObject");
 
         if(staticObject)
         {
             staticRenderer = staticObject.GetComponent<Renderer>();
+
+            staticRenderer.material.color = new Color(0, 0, 0, 0);
         }
         else
         {
@@ -25,6 +33,22 @@ public class TestHealthScript : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-	
+        LoseHealth();
 	}
+
+    void LoseHealth()
+    {
+        float healthDecay = startingHealth / healthDecrease;
+
+        //health -= healthDecay * Time.deltaTime;
+
+        float newAlpha = 1.0f - health / startingHealth;
+
+        staticRenderer.material.color += new Color(1, 1, 1, newAlpha);
+
+        if (health <= 0)
+        {
+            Debug.Log("I died! :(");
+        }
+    }
 }
